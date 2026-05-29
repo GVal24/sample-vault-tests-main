dd72499ca3b6e40ecc5f63ba302ef90843668850
GVal24 <pcpnormalazul@gmail.com>
2026-05-28 22:14:08 -0300

    Pruebas 2 y 3

--- frontend/js/tests/authTests.js
+++ frontend/js/tests/authTests.js
@@ -87,26 +87,9 @@ testUtils.createTestButton("Test Registro - Usuario Nuevo", async (btn) => {
 });
 
 testUtils.createTestButton("Test Seguridad - Productor accediendo a Admin", async (btn) => {
-    async function okLogin() {
-        const response = await fetch('/api/auth/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username: 'pepe', password: '12345' })
-        });
-
-        const data = await response.json();
-        testUtils.log(data);
-
-        if (!response.ok || !data.token) {
-            return null;
-        }
-
-        localStorage.setItem('test_token', data.token);
-        return data.token;
-    }
-
+    
     const token = await okLogin();
-    if (!token) {
+     if (!token) {
         return;
     }
 
@@ -121,9 +104,47 @@ testUtils.createTestButton("Test Seguridad - Productor accediendo a Admin", asyn
     const data = await response.json();
     testUtils.log(data);
 
-    if (response.status === 403) {
+    if (response.status != 403) {
         testUtils.setSuccess(btn);
     }
 });
 
+testUtils.createTestButton("Test Eliminar Sample Dinámico", async (btn) => {
+    const token = await okLogin();
+     if (!token) {
+        return;
+    }
 
+    const samplesResponse = await fetch('/api/samples/my-samples', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+    const samplesData = await samplesResponse.json();
+
+    testUtils.log(samplesData);
+
+
+    if(!samplesData || samplesData.length === 0){
+        testUtils.log("No hay samples existentes. Se debe subir un sample primero");
+    } else{
+            const sampleId = samplesData[0].id;
+            const deleteResponse = await fetch(`/api/samples/${sampleId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            const data = await deleteResponse.json();
+
+            testUtils.log(`Intentando borrar el sample con ID: ${sampleId}`);
+
+            if (deleteResponse.ok) {
+                testUtils.setSuccess(btn);
+            }
+    }
+
+});
